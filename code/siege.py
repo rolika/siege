1e82c83e0594adb86fd644c533906576cd7ae1df
Weisz Roland <weisz.roland@wevik.hu>
2021-08-06 17:20:01 +0200

    add main game class

--- code/siege.py
+++ code/siege.py
@@ -1,4 +1,20 @@
+"""Siege
+Siege is a game about a lonely hero who defends his castle from attackers by hurling barrels at them.
+Code is loosely based on ClearCode's Space Invaders tutorial found here: https://www.youtube.com/watch?v=o-6pADy5Mdg
+"""
+
+
 import pygame, sys
+
+
+class Siege:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+        # update all sprite groups
+        # draw all sprite groups
 
 
 if __name__ == "__main__":
@@ -7,6 +23,7 @@ if __name__ == "__main__":
     screen_height = 600
     screen = pygame.display.set_mode((screen_width, screen_height))
     clock = pygame.time.Clock()
+    game = Siege()
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +32,7 @@ if __name__ == "__main__":
                 sys.exit()
         
         screen.fill((30, 30, 30))
+        game.run()
 
         pygame.display.flip()
         clock.tick(60)
