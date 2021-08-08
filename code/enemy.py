3802e1b458855bbc511c2131cc8b1b4b06275519
Weisz Roland <weisz.roland@wevik.hu>
2021-08-08 11:14:26 +0200

    attacker climbs

--- code/enemy.py
+++ code/enemy.py
@@ -1,5 +1,6 @@
 """In Siege!, enemies are coming in at ground level, from left or right.
-Once they reach the bastion, they start to climb up the walls at a randomized place."""
+Once they reach the bastion, they start to climb up the walls. The wall is divided into 7 lanes.
+The attacker chooses the nearest ladder with the least other attackers already on it at spawn time."""
 
 
 from pygame import sprite, Surface
@@ -8,12 +9,13 @@ from constant import ENEMY_CLIMBING_SPEED, ENEMY_WALKING_SPEED, GROUND_LEVEL, SC
 
 
 class Enemy(sprite.Sprite):
-    def __init__(self) -> None:
+    def __init__(self, ladder=1) -> None:
         super().__init__()
         random.seed()
         self.image = Surface((20, 50))
         self.image.fill("red")
         self._spawn()
+        self._ladder = ladder
 
     def _spawn(self):
         direction = random.choice((-1, 1))  # -1: right side, 1: left side
@@ -24,6 +26,11 @@ class Enemy(sprite.Sprite):
     def _climb(self):
         self._speed = (0, ENEMY_CLIMBING_SPEED)
     
+    def _check_ladder(self):
+        return self.rect.centerx == 400
+    
     def update(self, *args, **kwargs) -> None:
         self.rect.x += self._speed[0]
         self.rect.y += self._speed[1]
+        if self._check_ladder():
+            self._climb()
