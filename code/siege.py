cc3d078916dd0dc4fa401114cbb70d50e0e3de05
Weisz Roland <weisz.roland@wevik.hu>
2021-08-11 18:11:04 +0200

    manage hi-score

--- code/siege.py
+++ code/siege.py
@@ -4,19 +4,21 @@ Code is loosely based on ClearCode's Space Invaders tutorial found here: https:/
 """
 
 
-from text import HiScore, Score, Text
 import pygame
 from pygame import sprite
 import sys
+import shelve
 from player import Player
 from scenery import Sky, Field, Bastion, Roof, LeftTower, RightTower, Road
 from enemy import Enemies
 from ladder import Ladders
-from constant import SCREEN_SIZE, BLUE_SKY, SCREEN_WIDTH
+from text import HiScore, Score, Text
+from constant import HISCORE_FILENAME, SCREEN_SIZE, BLUE_SKY, SCREEN_WIDTH
 
 
 class Siege:
     def __init__(self):
+        self._restore_hiscore()
         left_tower = LeftTower()
         right_tower = RightTower()
         self._scenery = sprite.Group(Sky(), Field(), Bastion(), left_tower, right_tower, Roof(left_tower.rect.midtop), Roof(right_tower.rect.midtop), Road())
@@ -26,15 +28,26 @@ class Siege:
         self._title = sprite.GroupSingle(Text("Siege!", "font/RubikMonoOne-Regular.ttf", 24, "darkslategrey", (SCREEN_WIDTH//2, 0)))
         self._score = sprite.GroupSingle(Score("font/Monofett-Regular.ttf", 24, "darkslategrey", (0, 0)))
         self._hiscore = sprite.GroupSingle(HiScore("font/Monofett-Regular.ttf", 24, "darkslategrey", (SCREEN_WIDTH, 0)))
+    
+    def _restore_hiscore(self):
+        with shelve.open(HISCORE_FILENAME) as hs:
+            self._hiscore_value = hs.get("hiscore", 0)
+    
+    def _save_hiscore(self):
+        with shelve.open(HISCORE_FILENAME) as hs:
+            hs["hiscore"] = self._hiscore_value
 
     def run(self, screen):
         # update sprites
         self._hero.update()
         self._enemies.update(self._hero.sprite.thrown_barrels)
         self._score.update(self._enemies.score)
-        self._hiscore.update()
+        if self._enemies.score > self._hiscore_value:
+            self._hiscore_value = self._enemies.score
+        self._hiscore.update(self._hiscore_value)
 
         if self._enemies.conquer:
+            self._save_hiscore()
             pygame.quit()
             sys.exit("Your castle has been conquered!")
         
