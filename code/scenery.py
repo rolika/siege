5f5747953c79f80b7fd725a9fe9cfa1cf65ba451
Weisz Roland <weisz.roland@wevik.hu>
2021-08-15 16:55:22 +0200

    redraw scenery

--- code/scenery.py
+++ code/scenery.py
@@ -29,14 +29,14 @@ class Bastion(sprite.Sprite):
 class LeftTower(sprite.Sprite):
     def __init__(self) -> None:
         super().__init__()
-        self.image = image.load("gfx/tower.png").convert()
+        self.image = image.load("gfx/tower_left.png").convert()
         self.rect = self.image.get_rect(bottomright=(LEFT_TOWER, GROUND_LEVEL))
 
 
 class RightTower(sprite.Sprite):
     def __init__(self) -> None:
         super().__init__()
-        self.image = image.load("gfx/tower.png").convert()
+        self.image = image.load("gfx/tower_right.png").convert()
         self.rect = self.image.get_rect(bottomleft=(RIGHT_TOWER, GROUND_LEVEL))
 
 
