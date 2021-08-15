d0cc04b4d223ba12a35ac6fdf948e24e6758e377
Weisz Roland <weisz.roland@wevik.hu>
2021-08-15 11:16:37 +0200

    add scenery graphics

--- code/ladder.py
+++ code/ladder.py
@@ -1,15 +1,14 @@
 """Enemies use ladders to climb the bastion. The ladders are evenly distributed over the bastion."""
 
 
-from pygame import sprite, Surface
+from pygame import sprite, image
 from constant import FIRST_LADDER_LEFT_POS, GROUND_LEVEL, LADDERS, LADDER_HEIGHT, LADDER_WIDTH, SPACE_BETWEEN_LADDERS
 
 
 class Ladder(sprite.Sprite):
     def __init__(self, x) -> None:
         super().__init__()
-        self.image = Surface((LADDER_WIDTH, LADDER_HEIGHT))
-        self.image.fill("grey")
+        self.image = image.load("gfx/ladder.png").convert_alpha()
         self.rect = self.image.get_rect(midbottom=(x, GROUND_LEVEL))
 
 
